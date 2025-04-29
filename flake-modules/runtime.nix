a5d13d91663074b0d8e948392afef7cba108e8f2
Yang, Bo <bo@preemo.io>
2025-04-29 08:08:30 +0000

    refactor: improve formatting and structure of runtime configuration in runtime.nix

--- flake-modules/runtime.nix
+++ flake-modules/runtime.nix
@@ -1,4 +1,10 @@
-topLevel@{ flake-parts-lib, inputs, lib, ... }: {
+topLevel@{
+  flake-parts-lib,
+  inputs,
+  lib,
+  ...
+}:
+{
   imports = [
     ./common.nix
     ./systems.nix
@@ -11,41 +17,44 @@ topLevel@{ flake-parts-lib, inputs, lib, ... }: {
       topLevel.config.flake.flakeModules.systems
       topLevel.config.flake.flakeModules.nixpkgs
     ];
-    options.perSystem = flake-parts-lib.mkPerSystemOption (perSystem@{ pkgs, ... }: {
-      options.ml-ops.runtime = lib.mkOption {
-        description = ''
-          Common config shared among all `ml-ops.jobs.<name>` and `ml-ops.services.<name>`.
-        '';
-        type = lib.types.deferredModuleWith {
-          staticModules = [
-            (runtime: {
-              imports = [ perSystem.config.ml-ops.common ];
-              options.name = lib.mkOption {
-                type = lib.types.str;
-                default = runtime.config._module.args.name;
-              };
-              config.devenvShellModule.name = runtime.config.name;
-              config.devenvShellModule.devenv.root = "/env";
-              config.devenvShellModule.process.managers.process-compose.tui.enable = false;
-              options.launcher = lib.mkOption {
-                type = lib.types.deferredModuleWith {
-                  staticModules = [
-                    {
-                      options.environmentVariables = lib.mkOption {
-                        type = lib.types.attrsOf lib.types.str;
-                      };
-                      config.environmentVariables = runtime.config.environmentVariables;
-                    }
-                  ];
+    options.perSystem = flake-parts-lib.mkPerSystemOption (
+      perSystem@{ pkgs, ... }:
+      {
+        options.ml-ops.runtime = lib.mkOption {
+          description = ''
+            Common config shared among all `ml-ops.jobs.<name>` and `ml-ops.services.<name>`.
+          '';
+          type = lib.types.deferredModuleWith {
+            staticModules = [
+              (runtime: {
+                imports = [ perSystem.config.ml-ops.common ];
+                options.name = lib.mkOption {
+                  type = lib.types.str;
+                  default = runtime.config._module.args.name;
                 };
-                default = { };
-              };
-            })
-          ];
-        };
-        default = { };
+                config.devenvShellModule.name = runtime.config.name;
+                config.devenvShellModule.devenv.root = "/env";
+                config.devenvShellModule.process.managers.process-compose.tui.enable = false;
+                options.launcher = lib.mkOption {
+                  type = lib.types.deferredModuleWith {
+                    staticModules = [
+                      {
+                        options.environmentVariables = lib.mkOption {
+                          type = lib.types.attrsOf lib.types.str;
+                        };
+                        config.environmentVariables = runtime.config.environmentVariables;
+                      }
+                    ];
+                  };
+                  default = { };
+                };
+              })
+            ];
+          };
+          default = { };
 
-      };
-    });
+        };
+      }
+    );
   };
 }
